4a2a295b5ad6f7fa76f59217d17fb101025c0e0f
Amy Hsieh <hahsieh@outlook.com>
2019-11-17 15:29:17 -0800

    intial version

---
+++ BSTInsert/BinarySearchTree.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using static System.Console;
+
+namespace BinarySearchTreeRecursive
+{
+    public class BinarySearchTree
+    {
+        internal class Node
+        {
+            public string Key;
+            public string Value;
+            public Node Left;
+            public Node Right;           
+        }
+
+        internal Node Root;
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        //Recursive Insert routine.  Public part starts at root and starts
+        //the recursion to find the node after which to put the new node.
+
+
+        public void Insert(string key, string value)
+        {
+            Root = Insert(Root, key, value);
+        }
+        private Node Insert(Node node, string key, string value)
+        {
+            if (node == null)           //We've gone past the end (or the tree is empty).
+                                        //Now we can add a leaf!
+            {
+                node = new Node();
+                node.Key = key;
+                node.Value = value;                
+            }    
+            else if (string.Compare(key, node.Key) == -1)    //This new node belongs somewhere left of where we are
+            {
+                node.Left = Insert(node.Left, key, value);
+            }
+            else                         //Otherwise, it's gottta be somewhere right of where we are
+            {
+                node.Right = Insert(node.Right, key, value);
+            }
+            return node;
+        }
+
+        private Node BalancedTree(Node node)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Traversal in order is like magic.  Can you work out what's happening?
+        public void Traverse()
+        {
+            Traverse(Root);
+        }
+        private void Traverse(Node node)
+        {
+            if (node == null) return;            
+            Traverse(node.Left);
+            WriteLine("- " + string.Format("{0,-14}", node.Key) + "(" + node.Value + ")");
+            Traverse(node.Right);
+        }
+
+        public void Delete(string key)
+        {
+            Root = Delete(Root, key);
+        }
+
+        private Node Delete(Node node, string key)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+            if (string.Compare(key, node.Key) == -1)
+            {
+                node.Left = Delete(node.Left, key);
+            }
+            else if (string.Compare(key, node.Key) == 1)
+            {
+                node.Right = Delete(node.Right, key);
+            }
+            else
+            {
+                //Case where node has zero or one child.  Just delete it.
+                if (node.Right == null)
+                {
+                    return node.Left;
+                }
+                else if (node.Left == null)
+                {
+                    return node.Right;
+                }
+
+                //For a node with two children, you replace the node being deleted with 
+                //the largest node in its smaller (left) subtree.
+
+                node.Key = MaxLeftChildValue(node.Left);
+
+                node.Left = Delete(node.Left, node.Key);                
+            }
+            
+            return node;
+        }
+
+        private string MaxLeftChildValue(Node node)
+        {
+            string maxVal = node.Key;
+            while (node.Right != null)
+            {
+                maxVal = node.Right.Key;
+                node = node.Right;
+            }
+
+            return maxVal;
+        }
+
+        public string Find(string key)
+        {
+            return Find(Root, key);
+        }
+
+        private string Find(Node node, string key)
+        {
+            if (node == null) return null;
+
+            if (key == node.Key) return node.Value;
+
+            if (string.Compare(key, node.Key) == 1)
+            {
+                node = node.Right;
+                return Find(node, key);                
+            }
+            else
+            {
+                node = node.Left;
+                return Find(node, key);
+            }
+        }        
+    }
+}
---
+++ BSTInsert/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BinarySearchTreeRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree t = new BinarySearchTree();
+            string inputName;
+            string inputLocation;
+            string result;
+            bool done = false;
+
+            List<string> teamNames = LoadTeamTable();
+            List<string> teamLocations = LoadTeamLocation();
+            for (int i = 0; i < teamNames.Count; i++)
+            {
+                t.Insert(teamNames[i], teamLocations[i]);
+                WriteLine($"{teamLocations[i]} {teamNames[i]} inserted.");
+            }
+
+            WriteLine();
+
+            do
+            {
+                Write("\nSelect (I)nsert, (D)elete, (T)raverse, (F)ind, or (Q)uit: ");
+                switch (ReadLine().ToUpper())
+                {
+                    case "I":
+                        WriteLine("\nINSERT A NEW TEAM");
+                        WriteLine("----------------------------------");
+                        Write("- Team name: ");
+                        inputName = ReadLine();
+                        Write("- Team city: ");
+                        inputLocation = ReadLine();
+                        t.Insert(inputName, inputLocation);
+                        WriteLine($"{inputName} successfully added.");
+                        teamNames.Add(inputName);
+                        teamLocations.Add(inputLocation);
+                        break;
+                    case "D":
+                        WriteLine("\nDELETE TEAM");
+                        WriteLine("----------------------------------"); 
+                        Write("- Team name: ");
+                        inputName = ReadLine();
+                        if (!teamNames.Contains(inputName))
+                        {
+                            WriteLine($"{inputName} not found.");
+                            break;
+                        }                                
+                        else
+                        {
+                            t.Delete(inputName);
+                            WriteLine($"{inputName} from {teamLocations[teamNames.IndexOf(inputName)]} are no more.");
+                        }                        
+                        break;
+                    case "T":
+                        WriteLine("\nTEAM NAMES IN ASCENDING SEQUENCE:");
+                        WriteLine("----------------------------------");
+                        t.Traverse();
+                        WriteLine();
+                        break;
+                    case "F":
+                        WriteLine("\nFIND A TEAM");
+                        WriteLine("----------------------------------"); 
+                        Write("- Team name: ");
+                        inputName = ReadLine();
+                        result = t.Find(inputName);
+                        if (result==null)
+                        {
+                            WriteLine($"{inputName} not found.");
+                        }
+                        else
+                        {
+                            WriteLine($"{inputName} are from {result}");
+                        }                                          
+                        break;
+                    case "Q":
+                        WriteLine("\nOkey okey.  All done.");
+                        done = true;
+                        break;
+                    default:
+                        WriteLine("Invalid command.");
+                        break;
+                }
+            } while (!done);
+
+            ReadKey();
+        }
+
+        private static List<string> LoadTeamTable()
+        {
+            return new List<string> {"Cardinals","Falcons","Ravens","Bills",
+                "Panthers","Bears","Bengals","Browns",
+                "Cowboys","Broncos","Lions","Packers",
+                "Texans","Colts","Jaguars","Chiefs",
+                "Chargers","Rams","Dolphins","Vikings",
+                "Patriots","Saints","Giants","Jets",
+                "Raiders","Eagles","Steelers","49ers",
+                "Seahawks","Buccaneers","Titans","Redskins"};
+        }
+
+        private static List<string> LoadTeamLocation()
+        {
+            return new List<string> {"Arizona","Atlanta","Baltimore","Buffalo",
+            "Carolina","Chicago","Cincinnati","Cleveland",
+            "Dallas","Denver","Detroit","Green Bay",
+            "Houston","Indianapolis","Jacksonville","Kansas City",
+            "Los Angeles","Los Angeles", "Miami","Minnesota",
+            "New England","New Orleans","New York","New York",
+            "Oakland","Philadelphia","Pittsburgh","San Francisco",
+            "Seattle","Tampa Bay","Tennessee", "Washington"};
+        }
+    }
+}
